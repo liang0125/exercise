2f1ed74fe6e46943b69f54500cc88a927abd2bc4
liang0125 <952787172@qq.com>
2019-02-23 20:31:52 +0800

    单线程聊天室0.0.2

--- chatroom/chat-room-client/src/main/java/singleClient/SingleThreadClient.java
+++ chatroom/chat-room-client/src/main/java/singleClient/SingleThreadClient.java
@@ -12,28 +12,65 @@ import java.util.Scanner;
  */
 
 public class SingleThreadClient {
+
+    private static OutputStreamWriter outputClient;
+    private  static InputStreamReader inputStreamReader;
+    private static Socket client;
+
     public static void main(String[] args) {
         int port=6666;
         String host="127.0.0.1";
         try {
             if(args.length>1){
                 port= Integer.parseInt(args[0]);
-                host=args[1];
+                if(args[1].split(".").length==4){
+                    host=args[1];
+                }
             }
-            Socket socket=new Socket(host,port);
-            System.out.println("服务端连接成功！！！");
-            System.out.println(socket.getInetAddress()+" "+socket.getPort());
 
-                Scanner inputClient = new Scanner(socket.getInputStream());
+            //第一次连接的提示信息
+            client=new Socket(host,port);
+            System.out.println("服务端连接成功！！！");
+
+                Scanner inputClient = new Scanner(client.getInputStream());
                 String in = inputClient.next();
                 System.out.println(in);
-                OutputStreamWriter outputClient = new OutputStreamWriter(socket.getOutputStream());
+                outputClient = new OutputStreamWriter(client.getOutputStream());
 
-                outputClient.write("你好服务器\n");
+                outputClient.write("连接到"+client.getInetAddress()+"地址的客户端,可以开始交互了！\n");
                 outputClient.flush();
+
+            Thread thread=new Thread(new Runnable() {
+                Scanner input=new Scanner(client.getInputStream());
+                public void run() {
+
+                    //接收数据
+                    while (true) {
+
+                        if (input.hasNext()) {
+                                System.out.println("服务端："+input.next());
+                            }
+                    }
+                }
+            });
+            thread.start();
+
+            //发送数据
+            Scanner ins=new Scanner(System.in);
+            while (true){
+                try {
+                    outputClient.write("客户端："+ins.next()+"\n");
+                    outputClient.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
     }
 
 }
--- chatroom/src/main/java/singleserver/SingleThreadServer.java
+++ chatroom/src/main/java/singleserver/SingleThreadServer.java
@@ -15,8 +15,9 @@ import java.util.Scanner;
  * Create:2019/2/23
  */
 public class SingleThreadServer {
-
-
+    private static OutputStreamWriter outputClient;
+    private  static InputStreamReader inputStreamReader;
+    private static Socket client;
     public static void main(String[] args) {
         int port = 6666;
 
@@ -27,22 +28,53 @@ public class SingleThreadServer {
             ServerSocket serverSocket = new ServerSocket(port);
 
             System.out.println("等待客户端的连接...");
-            System.out.println(serverSocket.getLocalPort());
             //客户端接收返回Socket对象
+                 client = serverSocket.accept();
 
-                Socket client = serverSocket.accept();
                 //来自客户端给服务端的输入
                 //接收数据
-                OutputStreamWriter outputClient = new OutputStreamWriter(client.getOutputStream());
+                 outputClient = new OutputStreamWriter(client.getOutputStream());
                 outputClient.write("你好，欢迎连接服务端！！！\n");
                 outputClient.flush();
                 Scanner inputClient = new Scanner(client.getInputStream());
                 String in = inputClient.next();
                 System.out.println(in);
 
+                //服务端线程
+            Thread thread=new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        Scanner input=new Scanner(client.getInputStream());
+                        while (true) {
+                            if (input.hasNext()) {
+                                System.out.println(input.next());
+
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            thread.start();
+
+            //发送数据
+            Scanner ins=new Scanner(System.in);
+            while (true){
+                if(ins.hasNext()){
+                    try {
+                        outputClient.write(ins.next()+"\n");
+                        outputClient.flush();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+
 
     }
 }
