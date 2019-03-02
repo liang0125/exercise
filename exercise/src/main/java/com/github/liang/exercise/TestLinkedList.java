f141ecfda9553c3b879df8e34a340a67b89bb090
liang0125 <952787172@qq.com>
2019-03-02 17:37:06 +0800

    日常练习

--- exercise/src/main/java/com/github/liang/exercise/TestLinkedList.java
+++ exercise/src/main/java/com/github/liang/exercise/TestLinkedList.java
@@ -18,39 +18,79 @@ class ListNode1 {
         this.val = val;
     }
 }
-public class TestLinkedList {
-    public static ListNode1 ReverseList(ListNode1 head) {
+//public class TestLinkedList {
+//    public static ListNode1 ReverseList(ListNode1 head) {
+//
+//        if(head==null){
+//            return null;
+//        }
+//        if(head.next==null){
+//            return head;
+//        }
+//        ListNode1 curr=head;//指向原链表的第一个结点
+//        ListNode1 delNode=null;
+//        ListNode1 news = null;
+//        while (curr != null)
+//        {
+//            //从原链表头删，delNode是从原链表删掉的结点
+//            delNode = curr;
+//            curr = curr.next;
+//            //头插到新结点
+//            delNode.next = news;
+//            news = delNode;
+//        }
+//        return news;
+//    }
+//
+//    public static void main(String[] args) {
+//       ListNode1 listNode1=new ListNode1(1);
+//       ListNode1 listNode2=new ListNode1(2);
+//       ListNode1 listNode3=new ListNode1(3);
+//       ListNode1 listNode4=new ListNode1(4);
+//       listNode1.next=listNode2;
+//       listNode2.next=listNode3;
+//       listNode3.next=listNode4;
+//
+//        System.out.println(ReverseList(listNode1).val);
+//    }
+//}
 
+
+/*
+题目描述
+输入一个链表，输出该链表中倒数第k个结点。
+ */
+public class TestLinkedList {
+    public static ListNode1 FindKthToTail(ListNode1 head, int k) {
         if(head==null){
             return null;
         }
-        if(head.next==null){
-            return head;
+
+        int len = 0;
+        ListNode1 curr=head;
+        while (curr!= null) {
+            len++;
+            curr=curr.next;
         }
-        ListNode1 curr=head;//指向原链表的第一个结点
-        ListNode1 delNode=null;
-        ListNode1 news = null;
-        while (curr != null)
-        {
-            //从原链表头删，delNode是从原链表删掉的结点
-            delNode = curr;
-            curr = curr.next;
-            //头插到新结点
-            delNode.next = news;
-            news = delNode;
+        if(k>len){
+            return null;
         }
-        return news;
+        curr=head;
+        for (int i = 0; i < len - k; i++) {
+
+            curr=curr.next;
+        }
+        return curr;
     }
 
     public static void main(String[] args) {
-       ListNode1 listNode1=new ListNode1(1);
-       ListNode1 listNode2=new ListNode1(2);
-       ListNode1 listNode3=new ListNode1(3);
-       ListNode1 listNode4=new ListNode1(4);
-       listNode1.next=listNode2;
-       listNode2.next=listNode3;
-       listNode3.next=listNode4;
-
-        System.out.println(ReverseList(listNode1).val);
+        ListNode1 listNode1 = new ListNode1(1);
+        ListNode1 listNode2 = new ListNode1(2);
+        ListNode1 listNode3 = new ListNode1(3);
+        ListNode1 listNode4 = new ListNode1(4);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        System.out.println(FindKthToTail(listNode1,2).val);
     }
 }
