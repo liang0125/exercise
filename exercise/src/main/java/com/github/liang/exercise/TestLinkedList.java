3a9d35b08193950ae57249c60e8c5b76731f4d69
liang0125 <952787172@qq.com>
2019-03-02 19:27:07 +0800

    日常练习

--- exercise/src/main/java/com/github/liang/exercise/TestLinkedList.java
+++ exercise/src/main/java/com/github/liang/exercise/TestLinkedList.java
@@ -60,37 +60,118 @@ class ListNode1 {
 题目描述
 输入一个链表，输出该链表中倒数第k个结点。
  */
+//public class TestLinkedList {
+//    public static ListNode1 FindKthToTail(ListNode1 head, int k) {
+//        if(head==null){
+//            return null;
+//        }
+//
+//        int len = 0;
+//        ListNode1 curr=head;
+//        while (curr!= null) {
+//            len++;
+//            curr=curr.next;
+//        }
+//        if(k>len){
+//            return null;
+//        }
+//        curr=head;
+//        for (int i = 0; i < len - k; i++) {
+//
+//            curr=curr.next;
+//        }
+//        return curr;
+//    }
+//
+//    public static void main(String[] args) {
+//        ListNode1 listNode1 = new ListNode1(1);
+//        ListNode1 listNode2 = new ListNode1(2);
+//        ListNode1 listNode3 = new ListNode1(3);
+//        ListNode1 listNode4 = new ListNode1(4);
+//        listNode1.next = listNode2;
+//        listNode2.next = listNode3;
+//        listNode3.next = listNode4;
+//        System.out.println(FindKthToTail(listNode1,2).val);
+//    }
+//}
+
+/*
+题目描述
+输入两个单调递增的链表，输出两个链表合成后的链表，当然我们需要合成后的链表满足单调不减规则。
+ */
+
 public class TestLinkedList {
-    public static ListNode1 FindKthToTail(ListNode1 head, int k) {
-        if(head==null){
+
+    public static ListNode1 Merge(ListNode1 list1,ListNode1 list2) {
+
+        if(list1==null&&list2==null){
             return null;
         }
+        if(list1==null){
+            return list2;
+        }
+        if(list2==null){
+            return list1;
+        }
+        ListNode1 curr1=list1;
+        ListNode1 curr2=list2;
+        ListNode1 news=null;
+        ListNode1 tail=null;
+        ListNode1 node=null;
+        while (curr1!=null&&curr2!=null) {
+            if (curr1.val < curr2.val) {
+                node = curr1;
+                curr1=curr1.next;
+                if (news == null) {
+                    news = node;
+                } else {
+                    tail.next = node;
+                }
+                node.next = null;
+                tail = node;
+            }
+            else {
+                node=curr2;
+                curr2=curr2.next;
+                if (news == null) {
+                    news = node;
+                } else {
+                    tail.next = node;
+                }
+                node.next = null;
+                tail = node;
+            }
+        }
 
-        int len = 0;
-        ListNode1 curr=head;
-        while (curr!= null) {
-            len++;
-            curr=curr.next;
+        if(curr1==null){
+            tail.next=curr2;
         }
-        if(k>len){
-            return null;
+        if(curr2==null){
+            tail.next=curr1;
         }
-        curr=head;
-        for (int i = 0; i < len - k; i++) {
-
-            curr=curr.next;
-        }
-        return curr;
+        return news;
     }
 
     public static void main(String[] args) {
         ListNode1 listNode1 = new ListNode1(1);
-        ListNode1 listNode2 = new ListNode1(2);
-        ListNode1 listNode3 = new ListNode1(3);
-        ListNode1 listNode4 = new ListNode1(4);
+        ListNode1 listNode2 = new ListNode1(3);
+        ListNode1 listNode3 = new ListNode1(5);
+        ListNode1 listNode4 = new ListNode1(7);
         listNode1.next = listNode2;
         listNode2.next = listNode3;
         listNode3.next = listNode4;
-        System.out.println(FindKthToTail(listNode1,2).val);
+        ListNode1 listNode5=new ListNode1(2);
+        ListNode1 listNode6=new ListNode1(4);
+        ListNode1 listNode7=new ListNode1(6);
+        ListNode1 listNode8=new ListNode1(8);
+        listNode5.next = listNode6;
+        listNode6.next = listNode7;
+        listNode7.next = listNode8;
+        ListNode1 listNode9=Merge(listNode1,listNode5);
+        ListNode1 cur=listNode9;
+        while (cur!=null){
+            System.out.println(cur.val);
+            cur=cur.next;
+        }
     }
 }
