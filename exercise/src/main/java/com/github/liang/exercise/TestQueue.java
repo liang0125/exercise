b6d3fbd0278ab3399406ab9c55904354a2d1a394
liang0125 <952787172@qq.com>
2019-03-02 14:50:34 +0800

    日常练习

--- exercise/src/main/java/com/github/liang/exercise/TestQueue.java
+++ exercise/src/main/java/com/github/liang/exercise/TestQueue.java
@@ -1,8 +1,49 @@
 package com.github.liang.exercise;
 
+import java.util.Stack;
+
 /**
  * Author:lorrie
  * Create:2019/3/1
  */
+/*
+题目描述：
+用两个栈来实现一个队列，完成队列的Push和Pop操作。
+队列中的元素为int类型。
+ */
 public class TestQueue {
+    static Stack<Integer> stack1 = new Stack<Integer>();
+    static Stack<Integer> stack2 = new Stack<Integer>();
+
+    public static void push(int node) {
+        //直接入到第一个栈
+        stack1.push(node);
+    }
+
+    public static int pop() {
+        while (!stack1.isEmpty()){
+            int data=stack1.pop();
+            stack2.push(data);
+        }
+        int rsdata=stack2.pop();
+        //每次进行出队操作要保证第二个栈是空的
+        while(!stack2.isEmpty()){
+            int data =stack2.pop();
+            stack1.push(data);
+        }
+        return rsdata;
+    }
+
+    public static void main(String[] args) {
+     push(1);
+     push(2);
+     push(3);
+        System.out.println(pop());
+        System.out.println(pop());
+     push(4);
+        System.out.println(pop());
+     push(5);
+        System.out.println(pop());
+        System.out.println(pop());
+    }
 }
