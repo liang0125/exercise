2bd7e7454a29c91ff73cf041a6934d999673951d
liang0125 <952787172@qq.com>
2019-03-02 15:23:18 +0800

    日常练习

--- exercise/src/main/java/com/github/liang/exercise/TestFib.java
+++ exercise/src/main/java/com/github/liang/exercise/TestFib.java
@@ -22,16 +22,28 @@ public class TestFib {
 //    }
 
     //非递归
-    public static int fibonacci(int n) {
-       int [] arr=new int[n+1];
-       arr[0]=0;
-       arr[1]=1;
-       for(int i=2;i<=n;i++){
-           arr[i]=arr[i-1]+arr[i-2];
-       }
-       return arr[n];
-    }
+//    public static int fibonacci(int n) {
+//       int [] arr=new int[n+1];
+//       arr[0]=0;
+//       arr[1]=1;
+//       for(int i=2;i<=n;i++){
+//           arr[i]=arr[i-1]+arr[i-2];
+//       }
+//       return arr[n];
+//    }
 
+
+    public static int fibonacci(int n) {
+        int first=0;
+        int second=1;
+        int third=0;
+        for(int i=2;i<=n;i++){
+            third=first+second;
+            first=second;
+            second=third;
+        }
+        return third;
+    }
     public static void main(String[] args) {
         System.out.println(fibonacci(38));
     }
