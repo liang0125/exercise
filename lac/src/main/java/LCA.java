56d75cc6528522973cbafb2fb6908761709e4e42
liang0125 <952787172@qq.com>
2019-03-11 20:30:41 +0800

    每日一题2019/03/11

---
+++ lac/src/main/java/LCA.java
@@ -0,0 +1,80 @@
+/**
+ * Author:lorrie
+ * Create:2019/3/11
+ */
+import java.util.*;
+
+/*
+题目描述
+求公共祖先，满二叉树，根节点是1，给出两个数，求这两个数的最近公共祖先
+ */
+public class LCA {
+//    public static int getLCA(int a, int b) {
+//        if(a==1||b==1){
+//            return 1;
+//        }
+//        if (a==b){
+//            return a;
+//        }
+//        else {
+//            int x=getLCA(a/2,b);
+//            int y=getLCA(a,b/2);
+//            if (x>y){
+//                return x;
+//            }
+//            else {return y;}
+//        }
+//    }
+
+    public static int getLCA(int a, int b) {
+       while (a!=b){
+           if (a>b){
+               a=a/2;
+           }
+           else {
+               b=b/2;
+           }
+       }
+       return a;
+    }
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int a=in.nextInt();
+        int b=in.nextInt();
+        System.out.println(getLCA(a,b));
+    }
+
+    /*
+    题目描述
+    字符串反转
+     */
+
+//    public static String reverse(String str){
+//        if (str==null){
+//            return null;
+//        }
+//        if (str.equals("")){
+//            return "";
+//        }
+//      else {
+//            char[] c = str.toCharArray();
+//            int begin = 0;
+//            int end = c.length - 1;
+//            while (begin < end) {
+//                char t = c[begin];
+//                c[begin] = c[end];
+//                c[end] = t;
+//                begin++;
+//                end--;
+//            }
+//            String newStr = new String(c);
+//            return newStr;
+//        }
+//    }
+//    public static void main(String[] args) {
+//        Scanner in=new Scanner(System.in);
+//        String s=in.nextLine();
+//        System.out.println(reverse(s));
+//    }
+}
